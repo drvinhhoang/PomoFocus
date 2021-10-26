88d576b2030ec5dc58058012eb52705dd54f7403
vinh <dr.vinhhoang@gmail.com>
2021-10-26 21:50:23 +0700

    Initial Commit

---
+++ LetsFocusPomoSPM/LetsFocusPomoSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsFocusPomoSPMApp.swift
+//  LetsFocusPomoSPM
+//
+//  Created by VINH HOANG on 26/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct LetsFocusPomoSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
